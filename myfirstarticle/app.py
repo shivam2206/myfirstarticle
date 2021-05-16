61b05ba2119031d9513a48df3978c47309d6c171
Shivam Saxena <shivamsaxena2206@gmail.com>
2021-05-16 15:42:02 +0530

    Home page support using blueprints.
    The blog template used in this project is picked from https://startbootstrap.com/theme/clean-blog

--- myfirstarticle/app.py
+++ myfirstarticle/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_apispec.extension import FlaskApiSpec
 from flask_restful import Api
 
@@ -6,22 +6,33 @@ from myfirstarticle import settings
 from .api.routes import initialize_routes
 from .database import db, migrate
 from .utils.error_handler import handle_errors
+from .home import home
+import os
 
 
 def create_app(config=None):
     """This function creates the Flask app and handles all the initial configuration"""
-    app = Flask(settings.APP_NAME)
+
+    # TODO: Check why flask is not able to pick templates folder without specifying like below
+    app = Flask(settings.APP_NAME,
+                template_folder=os.path.join(os.path.dirname(__file__), 'templates'),
+                static_folder=os.path.join(os.path.dirname(__file__), 'static')
+                )
     app.config.from_object(settings)
 
     # Apply any modified config
     app.config.from_object(config)
     db.init_app(app)
     migrate.init_app(app, db)
-    api = Api(app, prefix='/api/v1')
-    docs = FlaskApiSpec(app)
 
     handle_errors(app)
+
+    # Register blueprints
+    app.register_blueprint(home)
+
+    # Add API support
+    api = Api(app, prefix='/api/v1')
+    docs = FlaskApiSpec(app)
     initialize_routes(api, docs)
 
     return app
-
